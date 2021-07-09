0394d2464cf286d9c35cda59f624615396f36bc3
Simon Chao <ysc406@nyu.edu>
2021-07-09 18:30:29 -0400

    Adding conditional modifier to Total amount Text View, red if choosing 0% tip, black otherwise.

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -9,17 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var checkAmount = ""
-    @State private var numberOfPeople = 2
+    @State private var numberOfPeople = ""
     @State private var tipPercentage = 2
     
     let tipPercentages = [10, 15, 20, 25, 0]
     
-    var totalPerPerson: Double {
-        let peopleCount = Double(numberOfPeople + 2)
+    var totalAmount: Double {
         let tipSelection = Double(tipPercentages[tipPercentage])
         let orderAmount = Double(checkAmount) ?? 0
         
-        return orderAmount * (1 + tipSelection / 100) / peopleCount
+        return orderAmount * (1 + tipSelection / 100)
+    }
+    
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople) ?? 1
+        
+        return totalAmount / peopleCount
     }
     
     var body: some View {
@@ -29,11 +34,13 @@ struct ContentView: View {
                     TextField("Amount", text: $checkAmount)
                         .keyboardType(.decimalPad)
                     
-                    Picker("Number of people", selection: $numberOfPeople) {
-                        ForEach(2 ..< 100) {
-                            Text("\($0) people")
-                        }
-                    }
+//                    Picker("Number of people", selection: $numberOfPeople) {
+//                        ForEach(2 ..< 100) {
+//                            Text("\($0) people")
+//                        }
+//                    }
+                    TextField("Number of people", text: $numberOfPeople)
+                        .keyboardType(.decimalPad)
                 }
                 
                 Section(header: Text("How much tip do you want to leave?")) {
@@ -46,9 +53,16 @@ struct ContentView: View {
                 }
                 .textCase(nil)
                 
-                Section {
+                Section(header: Text("Total Amount")) {
+                    Text("$\(totalAmount, specifier: "%.2f")")
+                        .foregroundColor(tipPercentage == 4 ? .red : .black)
+                }
+                .textCase(nil)
+                
+                Section(header: Text("Amount per person")) {
                     Text("$\(totalPerPerson, specifier: "%.2f")")
                 }
+                .textCase(nil)
             }
             .navigationBarTitle("WeSplit")
         }
